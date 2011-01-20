e4dbfc0902bef6568ff56f33d11695fff6fdbf4d
Elliot Winkler <elliot.winkler@gmail.com>
2011-01-19 23:57:59 -0700

    Add more tests which are failing atm

--- test/differ_test.rb
+++ test/differ_test.rb
@@ -6,11 +6,45 @@ module SuperDiff
       @differ = SuperDiff::Differ.new
     end
     
+    def test_same_strings
+      actual = @differ.diff("foo", "foo")
+      expected = {
+        :equal => true,
+        :expected => {:value => "foo", :type => :string},
+        :actual => {:value => "bar", :type => :string},
+        :same_class => true
+      }
+      assert_equal expected, actual
+    end
+    
     def test_differing_strings
       actual = @differ.diff("foo", "bar")
       expected = {
-        :expected => {:value => "foo", :class => String},
-        :actual => {:value => "bar", :class => String},
+        :equal => false,
+        :expected => {:value => "foo", :type => :string},
+        :actual => {:value => "bar", :type => :string},
+        :same_class => true
+      }
+      assert_equal expected, actual
+    end
+    
+    def test_differing_numbers
+      actual = @differ.diff(1, 1)
+      expected = {
+        :equal => true,
+        :expected => {:value => 1, :type => :number},
+        :actual => {:value => 2, :type => :number},
+        :same_class => true
+      }
+      assert_equal expected, actual
+    end
+    
+    def test_differing_numbers
+      actual = @differ.diff(1, 2)
+      expected = {
+        :equal => false,
+        :expected => {:value => 1, :type => :number},
+        :actual => {:value => 2, :type => :number},
         :same_class => true
       }
       assert_equal expected, actual
