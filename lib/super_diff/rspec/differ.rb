fe909c6589b4df7fef4bdea147a4143191688f86
Elliot Winkler <elliot.winkler@gmail.com>
2018-10-02 09:22:32 -0600

    Fix the RSpec differ so it's less noisy
    We don't need to show the diff after a failure for simple or primitive
    values.

--- lib/super_diff/rspec/differ.rb
+++ lib/super_diff/rspec/differ.rb
@@ -2,13 +2,28 @@ module SuperDiff
   module RSpec
     module Differ
       def self.diff(actual, expected)
-        diff = SuperDiff::Differ.call(
-          expected,
-          actual,
-          extra_operational_sequencer_classes: RSpec.extra_operational_sequencer_classes,
-          extra_diff_formatter_classes: RSpec.extra_diff_formatter_classes,
+        if (
+          expected != actual &&
+          expected.class == actual.class &&
+          !expected.is_a?(Symbol) &&
+          !expected.is_a?(Integer) &&
+          !(
+            expected.is_a?(String) &&
+            actual.is_a?(String) &&
+            !expected.include?("\n") &&
+            !actual.include?("\n")
+          )
         )
-        "\n\n" + diff
+          diff = SuperDiff::Differ.call(
+            expected,
+            actual,
+            extra_operational_sequencer_classes: RSpec.extra_operational_sequencer_classes,
+            extra_diff_formatter_classes: RSpec.extra_diff_formatter_classes,
+          )
+          "\n\n" + diff
+        else
+          ""
+        end
       end
     end
   end
