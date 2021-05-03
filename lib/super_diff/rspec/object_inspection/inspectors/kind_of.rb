5027bd7f2ee89bfddee8e21421064aefb2a3b075
wata_mac <watassbass@gmail.com>
2021-05-04 01:39:06 +0900

    Add support for kind_of argument matcher

--- lib/super_diff/rspec/object_inspection/inspectors/kind_of.rb
+++ lib/super_diff/rspec/object_inspection/inspectors/kind_of.rb
@@ -4,15 +4,20 @@ module SuperDiff
       module Inspectors
         class KindOf < SuperDiff::ObjectInspection::Inspectors::Base
           def self.applies_to?(value)
-            SuperDiff::RSpec.a_kind_of_something?(value)
+            SuperDiff::RSpec.a_kind_of_something?(value) || SuperDiff::RSpec.kind_of_something?(value)
           end
 
           protected
 
           def inspection_tree
             SuperDiff::ObjectInspection::InspectionTree.new do
-              add_text do |aliased_matcher|
-                "#<a kind of #{aliased_matcher.expected}>"
+              add_text do |value|
+                klass = if SuperDiff::RSpec.a_kind_of_something?(value)
+                          value.expected
+                        else
+                          value.instance_variable_get(:@klass)
+                        end
+                "#<a kind of #{klass}>"
               end
             end
           end
