55d9dc219771c67922dca5660bef1ee56baad0ec
Ransom Briggs <ransom.briggs@ezcater.com>
2022-04-07 11:39:18 -0500

    Change try to is_a?(Hash)

--- lib/super_diff/helpers.rb
+++ lib/super_diff/helpers.rb
@@ -52,9 +52,9 @@ module SuperDiff
 
       def object_address_for(object)
         # Sources: <https://bugs.ruby-lang.org/issues/15408> and <https://bugs.ruby-lang.org/issues/15626#Object-ID>
-        address = JSON.parse(ObjectSpace.dump(object)).try(:[], "address")
-        if address
-          "0x%016x" % Integer(address, 16)
+        json = JSON.parse(ObjectSpace.dump(object))
+        if json.is_a?(Hash)
+          "0x%016x" % Integer(json["address"], 16)
         else
           ""
         end
