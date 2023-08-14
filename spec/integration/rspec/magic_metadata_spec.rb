6a964ed8c6c2c085fd282919def074f8fe7ad7b4
Elliot Winkler <elliot.winkler@gmail.com>
2023-08-14 17:58:20 +0900

    Respect extra_failure_lines in RSpec metadata
    RSpec tests can have associated metadata, which is just a hash of
    content. One magic metadata property is `extra_failure_lines`. If this
    property is set, then RSpec will read its content and include it in the
    output it prints if the test fails.
    
    This property particularly comes into play in RSpec system specs, a
    wrapper around Rails system tests. If a system test fails, a screenshot
    is automatically taken of the page (provided the driver supports it) and
    a message is printed which details the path to the screenshot. RSpec's
    wrapper will capture output generated while the test is running and then
    store it in `extra_failure_lines`. By doing this, output (including the
    screenshot path, but potentially other information as well) is
    effectively moved to the spec failure message.
    
    This gem, as it patches key parts of `rspec-rails`, doesn't support
    `extra_failure_lines`, so anything in this metadata property is
    effectively discarded. This commit restores support.
    
    Co-authored-by: willnet <netwillnet@gmail.com>

---
+++ spec/integration/rspec/magic_metadata_spec.rb
@@ -0,0 +1,51 @@
+require "spec_helper"
+
+RSpec.describe "Integration with RSpec's magic metadata", type: :integration do
+  it "includes extra_failure_lines in failure messages" do
+    as_both_colored_and_uncolored do |color_enabled|
+      snippet = <<~TEST.strip
+          RSpec.describe "test" do
+            it { expect(true).to be(false) }
+
+            after do
+              RSpec.current_example.metadata[:extra_failure_lines] = "foo\nbar"
+            end
+          end
+        TEST
+      program =
+        make_plain_test_program(
+          snippet,
+          color_enabled: color_enabled,
+          preserve_as_whole_file: true
+        )
+
+      expected_output =
+        build_expected_output(
+          color_enabled: color_enabled,
+          test_name: "test is expected to equal false",
+          snippet: "it { expect(true).to be(false) }",
+          expectation:
+            proc do
+              line do
+                plain "Expected "
+                actual "true"
+                plain " to equal "
+                expected "false"
+                plain "."
+              end
+            end,
+          extra_failure_lines:
+            proc do
+              indent by: 5 do
+                line "foo"
+                line "bar"
+              end
+            end
+        )
+
+      expect(program).to produce_output_when_run(expected_output).in_color(
+        color_enabled
+      )
+    end
+  end
+end
