ec3bc4ec583669e9fb1ee46b5fe3d8c6e31f3f41
Joe Stein <joe@splitwise.com>
2024-09-13 22:59:18 -0700

    Simplify DataObject OperationTreeBuilder applies_to?

--- lib/super_diff/basic/operation_tree_builders/data_object.rb
+++ lib/super_diff/basic/operation_tree_builders/data_object.rb
@@ -3,12 +3,8 @@ module SuperDiff
     module OperationTreeBuilders
       class DataObject < CustomObject
         def self.applies_to?(expected, actual)
-          unless SuperDiff::Core::Helpers.ruby_version_matches?("~> 3.2")
-            return false
-          end
-
-          expected.class == actual.class && expected.is_a?(Data) &&
-            actual.is_a?(Data)
+          SuperDiff::Core::Helpers.ruby_version_matches?("~> 3.2") &&
+            expected.class == actual.class && expected.is_a?(Data)
         end
 
         protected
