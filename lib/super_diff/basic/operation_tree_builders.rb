ee3bda46b1a9c5fbc5cf2b4e8789f5b170f3858c
Joe Stein <569991+jas14@users.noreply.github.com>
2024-10-22 13:41:25 -0400

    Remove OperationTreeBuilders::RangeObject autoload

--- lib/super_diff/basic/operation_tree_builders.rb
+++ lib/super_diff/basic/operation_tree_builders.rb
@@ -22,8 +22,6 @@ module SuperDiff
       )
       autoload :TimeLike, "super_diff/basic/operation_tree_builders/time_like"
       autoload :DateLike, "super_diff/basic/operation_tree_builders/date_like"
-      autoload :RangeObject,
-               "super_diff/basic/operation_tree_builders/range_object"
 
       class Main
         def self.call(*args)
